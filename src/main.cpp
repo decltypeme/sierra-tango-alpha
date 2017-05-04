6a544c4b8a86b918faa21b2ff670b466727ca904
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2017-05-04 06:24:31 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,34 @@
 #include "netlist.h"
 #include "path_finding.h"
+#include <fstream>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     DAG g;
+    ifstream netlist(argv[1], std::ifstream::in);
+    if (netlist.is_open())
+    {
+        parse_netlist(netlist,DAG &g);
+    }
 
-    g.nodes.push_back(node("a",IN));
-    g.nodes.push_back(node("and",CELL));
-    g.nodes.push_back(node("or",CELL));
-    g.nodes.push_back(node("inr1",FFD));
-    g.nodes.push_back(node("outr1",FFQ));
-    g.nodes.push_back(node("inr2",FFD));
-    g.nodes.push_back(node("outr2",FFQ));
-    g.nodes.push_back(node("c",OUT));
-    g.join("a","and");
-    g.join("a","or");
-    g.join("and","inr1");
-    g.join("or","inr1");
-    g.join("inr1","outr1");
-    g.join("outr1","inr2");
-    g.join("inr2","outr2");
-    g.join("outr2","c");
+    //    g.nodes.push_back(node("a",IN));
+    //    g.nodes.push_back(node("and",CELL));
+    //    g.nodes.push_back(node("or",CELL));
+    //    g.nodes.push_back(node("inr1",FFD));
+    //    g.nodes.push_back(node("outr1",FFQ));
+    //    g.nodes.push_back(node("inr2",FFD));
+    //    g.nodes.push_back(node("outr2",FFQ));
+    //    g.nodes.push_back(node("c",OUT));
+    //    g.join("a","and");
+    //    g.join("a","or");
+    //    g.join("and","inr1");
+    //    g.join("or","inr1");
+    //    g.join("inr1","outr1");
+    //    g.join("outr1","inr2");
+    //    g.join("inr2","outr2");
+    //    g.join("outr2","c");
+
     vector<path> paths = get_paths_graph(g);
 
     cin.get();
--- src/netlist.h
+++ src/netlist.h
@@ -1,4 +1,11 @@
 #ifndef NETLIST_H
 #define NETLIST_H
+#include "DAG.h"
+#include <fstream>
+
+void parse_netlist(std::ifstream ifs, DAG &g)
+{
+
+}
 
 #endif // NETLIST_H
