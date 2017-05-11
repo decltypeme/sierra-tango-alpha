131ff6b6ff14f290b5aed96dd21544a4b6c977f8
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2017-05-12 00:12:04 +0200

    ..

---
+++ src/critical-path-finder.cpp
@@ -0,0 +1,10 @@
+#include "/home/hussam/Downloads/libertyparser-1.0/src/liberty.h"
+#include <fstream>
+using namespace std;
+using namespace utils;
+using namespace liberty;
+
+int main(int argc, char *argv[])
+{
+    Library l = parse("/home/hussam/sierra-tango-alpha/lib/osu035.lib");
+}
