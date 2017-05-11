eeb13554025de68bd999a09e67e27d650358b2b2
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2017-05-11 18:48:23 +0200

    nothing:)

--- src/std_lib.h
+++ src/std_lib.h
@@ -8,9 +8,18 @@ typedef float delay_t;
 
 class std_lib{
 public:
-    delay_t get_output_transition(string gate_type, delay_t input_transition, cap_t output_capacitance);
-    delay_t get_cell_delay(string gate_type, delay_t input_transition, cap_t output_capacitance);
-    delay_t interpolate(string gate_type, delay_t input_transition, cap_t output_capacitance);
+    delay_t get_output_transition(string gate_type, delay_t input_transition, cap_t output_capacitance)
+    {
+
+    }
+    delay_t get_cell_delay(string gate_type, delay_t input_transition, cap_t output_capacitance)
+    {
+
+    }
+    delay_t interpolate(string gate_type, delay_t input_transition, cap_t output_capacitance)
+    {
+    	
+    }
 };
 
 #endif // STD_LIB_H
