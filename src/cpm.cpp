8d47fa33dc6843a317184120de2ed86d16ae36eb
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2017-05-12 03:46:43 +0200

    for islam

--- src/cpm.cpp
+++ src/cpm.cpp
@@ -6,10 +6,15 @@ using namespace liberty;
 void put_AAT(const Library &l, DAG &g)
 {
 
-    for(node &n:g.nodes)
+    for(node &_n:g.nodes)
     {
-        for(edge &e:n.edges)
+        for(edge &_e:n.edges)
         {
+            //Step1
+            cap_t output_cap = get_input_pin_cap(*(_e.n)->name) + _e.net_capacitence;
+            *(_e.n)->input_transition_time=get_transtion_time(_n,_e,output_cap);
+
+            //Step2
 
         }
     }
