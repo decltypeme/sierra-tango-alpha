5d02a4dcd3a487be5bddf63c3e4efae00e463903
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2017-05-24 09:47:03 +0200

    =horayy

--- tests/sta.sh
+++
@@ -1,3 +0,0 @@
-cd netlist
-chmod +x ../../bin/sta
-for i in *.netlist.v ; do ../../bin/sta ../../lib/osu035-updated.lib "$i" "../caps/$i.cap" "../constraints/$i.const" "../clk.txt" "../reports/$i.txt";echo "Processsing file $i"; done
