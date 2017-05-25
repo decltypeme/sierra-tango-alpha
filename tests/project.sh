ab54f70a6fe8ebfe520f35d9e5ab97e1ada3e7a6
decltypeme <islamfm@aucegypt.edu>
2017-05-25 10:00:42 +0200

    Final isA?

--- tests/project.sh
+++ tests/project.sh
@@ -7,5 +7,6 @@ for i in *.netlist.v ;
 				../../bin/sta ../../lib/osu035-updated.lib $i ../caps/$i.cap ../constraints/$i.const ../clk.txt ../paths/$i.paths ../violations/$i.violations;
 		../../bin/violations ../../lib/osu035-updated.lib $i ../caps/$i.cap ../constraints/$i.const ../clk.txt ../paths/$i.paths ../violations/$i.violations;
 		../../bin/slacks ../../lib/osu035-updated.lib $i ../caps/$i.cap ../constraints/$i.const ../clk.txt ../graphs/$i.dot;
-		dot -Tpng ../graphs/$i.dot -o ../pngs/$i.png;
+		sed -n -i -e 's/([a-z]+)\[([0-9])\]/$1$2/' ../graphs/$i.dot;
+		#dot -Tpng ../graphs/$i.dot -o ../pngs/$i.png;
 		done
